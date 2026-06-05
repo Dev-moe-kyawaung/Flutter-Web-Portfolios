5ffdd75d33f29d2cd8153d25eea16f3fb29ac03c
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-06 00:41:25 +0630

    Add AppLinks class with social media URLs

---
+++ lib/core/constants/app_links.dart
@@ -0,0 +1,6 @@
+class AppLinks {
+  static const github = 'https://github.com/Dev-moe-kyawaung';
+  static const gravatar = 'https://gravatar.com/moekyawaung2026';
+  static const youtube = 'https://www.youtube.com/channel/UCuTXUguZb4xjeL2nX8WJG';
+  static const linkedin = 'https://www.linkedin.com/in/moe-kyaw-aung-2653093a1';
+}
